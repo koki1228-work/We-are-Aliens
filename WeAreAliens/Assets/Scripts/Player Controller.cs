16f04bc30d092e7747a76a73705f33115951742e
DaL <koki1228.work@gmail.com>
2024-05-26 18:07:18 +0900

    add: contents 62~73

---
+++ WeAreAliens/Assets/Scripts/Player Controller.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Debug.Log(horizontal);
+
+        float vertical = Input.GetAxis("Vertical");
+        Debug.Log(vertical);
+    }
+}
